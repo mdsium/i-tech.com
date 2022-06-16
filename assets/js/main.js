b5bf24b13ee99fa1d78292f77f9f5314463792d1
md sium <55883376+mdsium@users.noreply.github.com>
2022-06-16 08:00:08 +0600

    changed-02

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,36 @@
-  jQuery(document).ready(function($){
-    $(".homepage-slides").owlCarousel({
-      items: 1,
-      nav: false,
-      dots: true,
-      loop: true,
-      autoplay: false,
-      autoplayTimeout:2000,
-      autoplayHoverPause:true,
-
-    });
-    $(".projects-titles li").on('click', function() {
-
-      $(".projects-titles li").removeClass("active");
-      $(this).addClass("active");
-
-      var selector = $(this).attr('data-filter');
-      $(".projects-list").isotope({
-        filter: selector
-      });
-    });
-    jQuery(".projects-list").isotope();
-
-
-    $(".staff-list").owlCarousel({
-      items:4,
-      autoplay: false,
-      margin: 30,
-      loop:true,
-      autoplay: true,
-      autoplayTimeout:2000,
-      autoplayHoverPause:true,
-      nav: true,
-      navtaxt: ["<i class='fa fa-long-arrow-left'></i>" ,"<i class='fa fa-long-arrow-right'></i>"]
+jQuery(document).ready(function ($) {
+  $(".homepage-slides").owlCarousel({
+    items: 1,
+    nav: false,
+    dots: true,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
   });
-  
+  $(".projects-titles li").on("click", function () {
+    $(".projects-titles li").removeClass("active");
+    $(this).addClass("active");
+
+    var selector = $(this).attr("data-filter");
+    $(".projects-list").isotope({
+      filter: selector,
     });
+  });
+  jQuery(".projects-list").isotope();
 
-
+  $(".staff-list").owlCarousel({
+    items: 4,
+    autoplay: false,
+    margin: 30,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    nav: true,
+    navtaxt: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+  });
+  $(window).on("load", function(){
+    $(".preloader").fadeOut();
+  })
+});
